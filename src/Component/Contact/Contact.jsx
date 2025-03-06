01e1b813d26e114f650ac8ef1f442251144fd123
Nitesh Vashist <137484277+NiteshVash@users.noreply.github.com>
2025-03-06 18:06:18 +0530

    changes

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -1,5 +1,5 @@
-import React  , {useRef} from 'react'
-import './contact.css'
+import React, { useRef } from 'react';
+import './contact.css';
 import emailjs from '@emailjs/browser';
 
 function Contact() {
@@ -9,9 +9,7 @@ function Contact() {
         e.preventDefault();
 
         emailjs
-            .sendForm('service_9od457l', 'template_5osw4im', form.current, {
-                publicKey: '89b_F208I88tq0oAX1o-N',
-            })
+            .sendForm('service_9od457l', 'template_5osw4im', form.current, '89b_F208I88tq0oAX1o-N')
             .then(
                 () => {
                     console.log('SUCCESS!');
@@ -21,6 +19,7 @@ function Contact() {
                 },
             );
     };
+
     return (
         <section id='Contact' className='Contact'>
             <h1 className="Contactpagetitle">Contact</h1>
@@ -32,7 +31,7 @@ function Contact() {
                 <button className='submit' type="submit" value="Send" >Submit</button>
             </form>
         </section>
-    )
+    );
 }
 
-export default Contact
+export default Contact;
